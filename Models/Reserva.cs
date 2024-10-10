55915fdcad9f9a7cfaaf003d25f4e9884797722c
pozzii <contatopozzii@gmail.com>
2024-10-10 08:26:28 -0300

    Desafio concluídogit status

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,13 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            if (Suite.Capacidade >= hospedes.Count)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new ArgumentException("A quantidade de hospedes excede a capacidade permitida!");
             }
         }
 
@@ -35,23 +32,21 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            int quantidade = Hospedes.Count;
+            
+            return quantidade;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
-
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
+            int diasReservados = DiasReservados;
+            decimal valorDiaria = Suite.ValorDiaria;
+            decimal valor = diasReservados * valorDiaria;
+            
+            if (diasReservados >= 10)
             {
-                valor = 0;
+                decimal desconto = 0.10M;
+                valor = diasReservados * valorDiaria - (diasReservados * valorDiaria) * desconto;
             }
 
             return valor;
